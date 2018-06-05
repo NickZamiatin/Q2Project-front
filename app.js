f44729f920225b54ccd710d4d00cfa0eb217c2f5
Nikita Zamiatin <4406015@gmail.com>
2018-06-05 15:05:10 -0400

    lest start with show

--- app.js
+++ app.js
@@ -1,10 +1,79 @@
 window.addEventListener('load', () => {
     console.log('ready')
-    const baseUrl = 'http://localhost:3087/cookings'
+    const baseUrl = 'http://localhost:3087/cooking'
+
+        const createRecepi = () => {
+            event.preventDefault();
+            const name = document.querySelector('#name').value;
+            const ingredients = document.querySelector('#ingredients').value;
+            const preparation_time = document.querySelector('#preparation_time').value;
+            const country = document.querySelector('#country').value;
+            const cooking_img_url = document.querySelector('#cooking_img_url').value;
+            const gluten_free = document.querySelector('#gluten_free').value;
+            axios.post(baseUrl, {
+                  name,
+                  ingredients,
+                  preparation_time,
+                  country,
+                  cooking_img_url,
+                  gluten_free
+                })
+                .then(result => {
+                    showCooking(result.data)
+                })
+                .catch(error => {
+                    console.error(error)
+                })
+
+        }
+
+
 
     const newRecepi = () => {
         console.log('return add button ')
-        
+        // document.querySelector('#main-container').innerHTML = ""
+         document.querySelector('#app').innerHTML = `
+                         <div class="input">
+
+                         <button  class="btn btn-outline-warning btn-lg" type="submit" id='main-page' >Main page </button>
+                         <br/>
+                         <br/>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="btn btn-warning" id="inputGroup-sizing-default" id="name">Name</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                        <div class="input-group-prepend">
+                            <span class="btn btn-warning" id="inputGroup-sizing-default">Ingredients</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="btn btn-warning" id="inputGroup-sizing-default">Prep time</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                        <div class="input-group-prepend">
+                            <span class="btn btn-warning" id="inputGroup-sizing-default">Country</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <div class="input-group mb-3">
+                        <div class="input-group-prepend">
+                            <span class="btn btn-warning" id="inputGroup-sizing-default">Gluten free</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                        <div class="input-group-prepend">
+                            <span class="btn btn-warning" id="inputGroup-sizing-default">URL</span>
+                        </div>
+                        <input type="text" class="form-control" aria-label="Default" aria-describedby="inputGroup-sizing-default">
+                    </div>
+                    <button  class="btn btn-outline-warning btn-lg" type="submit"  id="create-recepi">Create</button>
+                </div>
+         `;
+             document.querySelector('#main-page').addEventListener('click', allRecepi)
+
+          document.querySelector('#create-recepi').addEventListener('click', createRecepi);          
     }
 
 
@@ -12,10 +81,10 @@ window.addEventListener('load', () => {
         document.querySelector('#app').innerHTML = `
           <div class="container">
               <div id="buttons">
-                  <button type="button" class="btn btn-outline-warning btn-lg id="new_recepi">ADD</button>
+                  <button type="button" class="btn btn-outline-warning btn-lg" id="new_recepi">ADD</button>
               </div>
           </div>
-          <div class="row">
+          <div class="row" id="main-container">
               <div class="col-12">
                   <table class="table table-hover table-dark">
                       <div class="zagolovok">
@@ -38,9 +107,10 @@ window.addEventListener('load', () => {
         document.querySelector('#new_recepi').addEventListener('click', newRecepi)
         axios.get(baseUrl)
             .then(cookings => {
+                console.log('Response array first item', cookings.data[0]);
                 cookings.data.forEach(cooking => {
                     const trEl = document.createElement('tr');
-                    trEl.innerHTML = ` <th scope="row">
+                    trEl.innerHTML = ` <th scope="row"  >
                                     <div class="form-check form-check-inline">
                                         <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
                                         <label class="form-check-label" for="inlineCheckbox1"></label>
@@ -70,8 +140,8 @@ window.addEventListener('load', () => {
                                     <button type="button" class="btn btn-warning">${cooking.gluten_free}</button>
                                 </td>
                                 <td>
-                                    <button type="button" class="btn btn-outline-warning"> add</button>
-                                    <button type="button" class="btn btn-outline-warning">delete </button>
+                                    <button type="button" class="btn btn-outline-warning" id=edit-${cooking.id}> add</button>
+                                    <button type="button" class="btn btn-outline-warning" id=delete-${cooking.id}>delete </button>
                                 </td>`;
                     document.querySelector('#recepi-tbody').appendChild(trEl);
                     document.querySelector(`#show-${cooking.id}`).addEventListener('click', () => {
@@ -90,17 +160,24 @@ window.addEventListener('load', () => {
             });
     }
 
-    const showCooking = () => {
+    const showCooking = cooking => {
         console.log('return show')
+        document.querySelector('#app').innerHTML = ``;
     }
     const editCooking = () => {
         console.log('return edit')
     }
-    const deleteCooking = () => {
-        console.log('return deelte')
+    const deleteCooking = id => {
+        console.log('delete move', id)
+        axios.delete(`${baseUrl}/${id}`)
+            .then(result => {
+                allRecepi();
+            })
+            .catch(error => {
+                console.error(error);
+            })
     }
 
-    document.querySelector('#app').addEventListener('click', allRecepi)
     allRecepi();
 
 })
