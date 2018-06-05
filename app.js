d4b23d2aac7c18894f3e8d920dfab8751f07404d
Nikita Zamiatin <4406015@gmail.com>
2018-06-05 10:14:42 -0400

    allRecepi

--- app.js
+++ app.js
@@ -1,38 +1,106 @@
-
-
-
 window.addEventListener('load', () => {
     console.log('ready')
     const baseUrl = 'http://localhost:3087/cookings'
 
     const newRecepi = () => {
-        console.log('Display new recepi ')
-        document.querySelector('#app').innerHTML = `
-    <form>
-        <div class="form-group">
-            <label for="title">Title</label>
-            <input type="text" id="title" class="form-control"/>
-        </div>
-        <div class="form-group">
-            <label for="year">Year</label>
-            <input type="text" id="year" class="form-control"/>
-        </div>
-
-        <div class="form-group">
-            <label for="director">Director</label>
-            <input type="text" id="director" class="form-control"/>
-        </div>
-        <div class="form-group">
-            <label for="rating">Rating</label>
-            <input type="text" id="rating" class="form-control"/>
-        </div>
-        <div class="form-group">
-            <label for="poster_url">Post URL</label>
-            <input type="text" id="poster_url" class="form-control"/>
-        </div>
-        <button type="submit" class="btn btn-primery" id="create-movie">Create</button>
-    </form>
-    `
-        document.querySelector('#create-movie').addEventListener('click', createMovie);
+        console.log('return add button ')
+        
     }
+
+
+    const allRecepi = () => {
+        document.querySelector('#app').innerHTML = `
+          <div class="container">
+              <div id="buttons">
+                  <button type="button" class="btn btn-outline-warning btn-lg id="new_recepi">ADD</button>
+              </div>
+          </div>
+          <div class="row">
+              <div class="col-12">
+                  <table class="table table-hover table-dark">
+                      <div class="zagolovok">
+                          <thead>
+                              <tr>
+                                  <th scope="col"></th>
+                                  <th scope="col">Name</th>
+                                  <th scope="col">Ingredients</th>
+                                  <th scope="col">Prep time</th>
+                                  <th scope="col">Country</th>
+                                  <th scope="col">Gluten free</th>
+                                  <th scope="col"> Action</th>
+
+                              </tr>
+                          </thead>
+                           <tbody id="recepi-tbody">
+                    </tbody>
+                      </div>
+        `;
+        document.querySelector('#new_recepi').addEventListener('click', newRecepi)
+        axios.get(baseUrl)
+            .then(cookings => {
+                cookings.data.forEach(cooking => {
+                    const trEl = document.createElement('tr');
+                    trEl.innerHTML = ` <th scope="row">
+                                    <div class="form-check form-check-inline">
+                                        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+                                        <label class="form-check-label" for="inlineCheckbox1"></label>
+                                    </div>
+                                </th>
+                                <td>
+                                    <button type="button" class="btn btn-warning" id=show-${cooking.id}>${cooking.name}</button>
+                                </td>
+                                <td>
+                                    <div class="btn-group" role="group">
+                                        <button id="btnGroupDrop1" type="button" class="btn btn-warning dropdown-toggle" data-toggle="dropdown" aria-haspopup="true"
+                                            aria-expanded="false">
+                                            Show all ingredients
+                                        </button>
+                                        <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                                            <p class="dropdown-item" href="#">${cooking.ingredients}</p>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-warning">${cooking.preparation_time}</button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-warning">${cooking.country}</button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-warning">${cooking.gluten_free}</button>
+                                </td>
+                                <td>
+                                    <button type="button" class="btn btn-outline-warning"> add</button>
+                                    <button type="button" class="btn btn-outline-warning">delete </button>
+                                </td>`;
+                    document.querySelector('#recepi-tbody').appendChild(trEl);
+                    document.querySelector(`#show-${cooking.id}`).addEventListener('click', () => {
+                        showCooking(cooking);
+                    })
+                    document.querySelector(`#edit-${cooking.id}`).addEventListener('click', () => {
+                        editCooking(cooking);
+                    })
+                    document.querySelector(`#delete-${cooking.id}`).addEventListener('click', () => {
+                        deleteCooking(cooking.id);
+                    })
+                });
+            })
+            .catch(error => {
+                console.error(error)
+            });
+    }
+
+    const showCooking = () => {
+        console.log('return show')
+    }
+    const editCooking = () => {
+        console.log('return edit')
+    }
+    const deleteCooking = () => {
+        console.log('return deelte')
+    }
+
+    document.querySelector('#app').addEventListener('click', allRecepi)
+    allRecepi();
+
 })
